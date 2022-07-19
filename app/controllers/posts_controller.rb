53f301084338e5dfe5ef4a6436d1caee3135b0e6
RogerSi666 <107162778+RogerSi666@users.noreply.github.com>
2022-07-19 16:06:53 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,10 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
 
-  # コメントアウトする
-  # def new
-  # end
-
- def create
-  Post.create(content: params[:content])
-  redirect_to action: :index  # 追記する
-end
-
+  def create
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
+  end
 end
