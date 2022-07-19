925eef6d290e034634e9d811a8e4902a684d59cf
RogerSi666 <107162778+RogerSi666@users.noreply.github.com>
2022-07-19 15:44:04 +0900

    メモ投稿機能

---
+++ app/javascript/memo.js
@@ -0,0 +1,14 @@
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
+
+window.addEventListener('load', post);
